02eb33565e5f67d3a656481a9573208b97e19ab0
Tony Chen Gu <chentony2011@hotmail.com>
2023-02-23 13:16:45 -0500

    apply candy's change

--- lib/page/upcoming_bookings_page.dart
+++ lib/page/upcoming_bookings_page.dart
@@ -8,8 +8,69 @@ class CurrentBookingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          AppBar(title: const Text('Search', style: TextStyle(fontSize: 30))),
+      appBar: AppBar(
+          title:
+              const Text('Upcoming Bookings:', style: TextStyle(fontSize: 25))),
+      body: Center(
+        child: ListView(
+          children: [
+            Container(
+              width: 390,
+              height: 400,
+              child: Card(
+                margin: EdgeInsets.all(8),
+                elevation: 10,
+                color: Color(0xB2AED9F9),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(children: [
+                    Text(
+                      'Swoop the Eagle',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      'Feb 30th, 2023',
+                      style: TextStyle(fontSize: 15, height: 2),
+                    ),
+                    Text(
+                      'Subject: ',
+                      style: TextStyle(fontSize: 20, height: 3),
+                    ),
+                  ]),
+                ),
+              ),
+            ),
+            Container(
+              width: 390,
+              height: 400,
+              child: Card(
+                margin: EdgeInsets.all(8),
+                elevation: 10,
+                color: Color(0xB2AED9F9),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(children: [
+                    Text(
+                      'Emory A',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      'Feb 30th, 2023',
+                      style: TextStyle(fontSize: 15, height: 2),
+                    ),
+                    Text(
+                      'Subject: ',
+                      style: TextStyle(fontSize: 20, height: 3),
+                    ),
+                  ]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
